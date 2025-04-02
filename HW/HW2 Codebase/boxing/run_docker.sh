6a768201859972b0a19d32c5d7566861df4a205b
Chan Hwang <chan@Chans-MacBook-Pro.local>
2025-04-01 20:54:34 -0400

    Docker

--- HW/HW2 Codebase/boxing/run_docker.sh
+++ HW/HW2 Codebase/boxing/run_docker.sh
@@ -10,26 +10,26 @@ BUILD=true  # Set this to true if you want to build the image
 
 # Check if we need to build the Docker image
 if [ "$BUILD" = true ]; then
-  echo "Building Docker image..."
+  echo "Building docker image"
   docker build -t ${IMAGE_NAME}:${CONTAINER_TAG} .
 else
-  echo "Skipping Docker image build..."
+  echo "Skipping docker image build"
 fi
 
 # Check if the database directory exists; if not, create it
 if [ ! -d "${DB_VOLUME_PATH}" ]; then
-  echo "Creating database directory at ${DB_VOLUME_PATH}..."
+  echo "Creating db directory at ${DB_VOLUME_PATH}..."
   mkdir -p "${DB_VOLUME_PATH}"
 fi
 
 # Stop and remove the running container if it exists
 if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
-    echo "Stopping running container: ${IMAGE_NAME}_container"
+    echo "Stop running container: ${IMAGE_NAME}_container"
     docker stop ${IMAGE_NAME}_container
 
     # Check if the stop was successful
     if [ $? -eq 0 ]; then
-        echo "Removing container: ${IMAGE_NAME}_container"
+        echo "Remove container: ${IMAGE_NAME}_container"
         docker rm ${IMAGE_NAME}_container
     else
         echo "Failed to stop container: ${IMAGE_NAME}_container"
@@ -48,4 +48,4 @@ docker run -d \
   -p ${HOST_PORT}:${CONTAINER_PORT} \
   ${IMAGE_NAME}:${CONTAINER_TAG}
 
-echo "Docker container is running on http://localhost:${HOST_PORT}"
+echo "Docker container is on http://localhost:${HOST_PORT}"
